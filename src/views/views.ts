55233e9299140517f5135134c166b06310a30467
05k4rausdertonne <oskarflath@gmail.com>
2022-01-20 15:19:01 +0100

    Add location detail route

--- src/views/views.ts
+++ src/views/views.ts
@@ -4,7 +4,7 @@ import { Cube } from "../types";
 // External imports
 import express from "express";
 import passport from "passport";
-import { getCubeWithId } from "../model/cube";
+import { getCubeWithId, getCubesByLocation } from "../model/cube";
 // Internal imports
 
 // Export the router
@@ -19,7 +19,7 @@ export var router: Router = express.Router();
 // })
 
 router.get('/:cubeId', getCubeDetail);
-
+router.get('/:location', getLocationDetail);
 
 async function getCubeDetail(req: Request, res: Response) {
 
@@ -30,6 +30,20 @@ async function getCubeDetail(req: Request, res: Response) {
             cubeId: cube.id
         });
     } catch (error) {
-        res.status(500);
+        res.status(500).end();
+    }
+}
+
+async function getLocationDetail(req: Request, res: Response) {
+
+    let location: string = req.params["location"];
+    try {
+        let cubes: Array<Cube> = await getCubesByLocation(location);
+        let cubeIds = cubes.map(e => {e.id});
+        res.render("cube-detail", {
+            cubeIds: cubeIds
+        });
+    } catch (error) {
+        res.status(500).end();
     }
 }
